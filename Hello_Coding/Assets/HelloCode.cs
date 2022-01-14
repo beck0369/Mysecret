8a28aa774ae1fa109ab8e06c7f888c1d6d821a42
Jinhyeon-Kim <0369beck@gmail.com>
2022-01-14 12:26:32 +0900

    Update HelloCode.cs
    과정 01

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class HelloCode : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World!");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }

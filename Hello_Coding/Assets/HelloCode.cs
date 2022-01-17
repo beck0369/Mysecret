65158126a3dcdce9d611c6230552676094ed62c0
Jinhyeon-Kim <0369beck@gmail.com>
2022-01-17 12:40:54 +0900

    20220117

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class HelloCode : MonoBehaviour
 {
-    void Start()
+    //void Start()
+    //{
+    //    Debug.Log("Hello World!");
+    //}  
+
+
+    //private void Start()
+    //{
+    //    string characterName = "라라";
+    //    char bloodType = 'A';
+    //    int age = 17;
+    //    float height = 168.3f;
+    //    bool isFemale = true;
+
+    //    Debug.Log("캐릭터 이름 :" + characterName);
+    //    Debug.Log("혈액형 :" + bloodType);
+    //    Debug.Log("나이 :" + age);
+    //    Debug.Log("키 :" + height);
+    //    Debug.Log("여성인가? :" + isFemale);
+
+    //}
+
+
+    private void Start()
     {
-        Debug.Log("Hello World!");
+        float distance = GetDistance(2, 2, 5, 6);
+        Debug.Log("(2, 2)에서 (5, 6)까지의 거리 : " + distance);
+    }
+    float GetDistance(float x1, float y1, float x2, float y2)
+    {
+        float width = x2 - x1;
+        float height = y2 - y1;
+
+        float distance = width * width + height * height;
+        distance = Mathf.Sqrt(distance);
+
+        return distance;
     }
 }

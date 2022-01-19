326e840ba6b40571d5b77a5a2fe4dddd49784aaa
Jinhyeon-Kim <0369beck@gmail.com>
2022-01-19 12:18:22 +0900

    1

--- Hello_Class/Assets/Scripts/Animal.cs
+++ Hello_Class/Assets/Scripts/Animal.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Animal  
 {
+    //동물에 대한 변수
     public string name;
     public string sound;
     
-    
+    //울음소리를 재생하는 메서드
     public void PlaySound()
     {
         Debug.Log(name + " : " + sound);
